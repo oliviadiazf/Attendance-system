bd5333254954a5733f89dfd9fcdabf106d756ef4
oliviadiazf <64765463+oliviadiazf@users.noreply.github.com>
2020-05-19 20:51:02 +0700

    Update Absen.php

--- app/Absen.php
+++ app/Absen.php
@@ -9,4 +9,6 @@ class Absen extends Model
     //
     protected $table = 'absen';
     protected $fillable = ['user_id','date','time_in','time_out','total_time','note_tugas','note_kendala'];
+    protected $_GET = ['time_in','time_out'];
+    protected $_POST = ['time_total'];
 }
